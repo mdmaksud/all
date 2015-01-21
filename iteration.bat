2465b4eae5ebbd741e4b8f5a1be17acd3962e5fb
maksud <maksud@eyeball.com>
2015-01-21 11:25:49 +0600

    Edited

--- iteration.bat
+++ iteration.bat
@@ -1,16 +1,12 @@
 @echo OFF
 
-
 @echo off
 FOR /L %%i IN (0 1 4000) DO (
 	echo Iteration:%%i  
-	tasklist | FIND "EyeballMessenger"   
+	tasklist | FIND "Skype.exe"
 	FOR /L %%j IN (0 1 30000) DO (
 		set t="MAKSUD"
-	)
-	
+	)	
 )    
 
 
-@tasklist | FIND "EyeballMessenger"
-@sleep 1
